e9aa821180a8c617d13e52911dfd3c573275b138
Tejas12malviya <91682736+Tejas12malviya@users.noreply.github.com>
2025-02-03 15:34:32 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,12 +4,13 @@ import requests
 app=FastAPI()
 
 API_URL="https://api.groq.com/openai/v1/chat/completions"
+API_Auth="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"
 
 @app.post("/call_API")
 def call(user_message:str):
     headers={
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"
+        "Authorization": "Bearer API_Auth"
     }
     payload = {
         "model": "llama-3.3-70b-versatile",
